48babf917fd08cfe825ac65b6cc3b2151940376d
Austin Du <austin@flare-app.com>
2017-01-06 12:05:21 -0500

    fixed buiildcontrol

---
+++ .tmp/concat/scripts/scripts.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name personalWebsite
+ * @description
+ * # personalWebsite
+ *
+ * Main module of the application.
+ */
+angular
+  .module('personalWebsite', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngMaterial',
+    'ngAria',
+    'ngMessages'
+  ])
+  .config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name personalWebsite.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the personalWebsite
+ */
+angular.module('personalWebsite')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+angular.module('personalWebsite').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('views/about.html',
+    "<p>This is the about view.</p>"
+  );
+
+
+  $templateCache.put('views/main.html',
+    "<div class=\"jumbotron\"> <h1>'Allo, 'Allo!</h1> <p class=\"lead\"> <img src=\"images/yeoman.png\" alt=\"I'm Yeoman\"><br> Always a pleasure scaffolding your apps. </p> <p><a class=\"btn btn-lg btn-success\" ng-href=\"#/\">Splendid!<span class=\"glyphicon glyphicon-ok\"></span></a></p> </div> <div class=\"row marketing\"> <h4>HTML5 Boilerplate</h4> <p> HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites. </p> <h4>Angular</h4> <p> AngularJS is a toolset for building the framework most suited to your application development. </p> <h4>Karma</h4> <p>Spectacular Test Runner for JavaScript.</p> </div>"
+  );
+
+}]);
