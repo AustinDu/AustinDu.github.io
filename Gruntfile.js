ab2166a6029d8912c6b7d6f0ad493fd88223d78d
Austin Du <austin@flare-app.com>
2017-01-06 13:08:58 -0500

    revert

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,28 @@ module.exports = function (grunt) {
     // Project settings
     yeoman: appConfig,
 
+    // grunt buildcontrol for deployment
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:AustinDu/AustinDu.github.io.git',
+          branch: 'gh-pages'
+        }
+      },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
+        }
+      }
+    },
+
     // Watches files for changes and runs tasks based on the changed files
     watch: {
       bower: {
@@ -65,26 +87,6 @@ module.exports = function (grunt) {
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
-      },
-      buildcontrol: {
-        options: {
-          dir: 'dist',
-          commit: true,
-          push: true,
-          message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-        },
-        pages: {
-          options: {
-            remote: 'git@github.com:AustinDu/AustinDu.github.io.git',
-            branch: 'gh-pages'
-          }
-        },
-        local: {
-          options: {
-            remote: '../',
-            branch: 'build'
-          }
-        }
       }
     },
 
